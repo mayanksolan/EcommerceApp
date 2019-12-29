f729bc19e35c43ef64c5720f59f7db3f315bd126
Mayank Solan <mayanksolan@gmail.com>
2019-12-30 00:28:22 +0530

    check changes

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -14,7 +14,7 @@ export default class Products extends Component {
       const products = await axios.get("/api/products");
       console.log(products);
       this.setState({ productsData: products.data }, () => {
-        console.log(this.state.productsData[0].specification.processor);
+        console.log(this.state.productsData[2].specification.processor);
       });
     } catch (error) {
       console.log(error);
@@ -30,7 +30,7 @@ export default class Products extends Component {
       return (
         <div>
           Here is some data:{" "}
-          {this.state.productsData[0].specification.processor}
+          {this.state.productsData[2].specification.processor}
         </div>
       );
     }
